236bed0be9866a48c983fd399c459b29c290b7a5
Zijun <s410126080@gmail.com>
2019-09-26 23:09:44 +0800

    動態輸出 burger ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,22 @@ import classes from './Burger.css';
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
+    // Object.keys 是由 js 原生提供的，他能夠將 Object 中的 key 取出來，
+    // 並且回傳由所有的 key 組成的 array
+
+    //將 ingredient object 的 key value 轉換成 burger ingredients 的 array
+    const transfromIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i)=>{
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            })
+        });
+
+    console.log(transfromIngredients);
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {transfromIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
