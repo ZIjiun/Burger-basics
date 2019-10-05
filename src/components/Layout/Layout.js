ecfa667e0df9249154645666a01e2d326e2d92d3
Zijun <s410126080@gmail.com>
2019-10-05 22:47:49 +0800

    重複使用 Backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/auxiliary';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-    <Aux>
-        <Toolbar/>
-        <SideDrawer/>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+// eslint-disable-next-line react/require-render-return
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    };
 
-export default layout;
+    sideDrawerCloseHandler = () => {
+        this.setState({showSideDrawer: false});
+    };
+
+    render() {
+      return (
+          <Aux>
+            <Toolbar/>
+            <SideDrawer
+                open={this.state.showSideDrawer}
+                closed={this.sideDrawerCloseHandler}/>
+            <main className={classes.Content}>
+               {this.props.children}
+            </main>
+          </Aux>
+      )
+    }
+}
+
+export default Layout;
